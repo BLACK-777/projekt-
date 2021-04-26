7537adb03d3fa16fd02ca67bbdb96cac53367e9c
BLACK-777 <75413781+BLACK-777@users.noreply.github.com>
2021-04-26 21:01:21 +0200

    Add files via upload

--- adresyIP.py
+++ adresyIP.py
@@ -3,17 +3,16 @@ import ipaddress
 import re
 
 # część zabezpieczająca przed podaniem złego adresu ip lub podania go w zlym formacie
-def check_regex():
+def is_proper_input(typed_value):
     reg = r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/(?:[1-9]|1[0-9]|2[0-9]|3[0-2])$"  #szablon adresu ip
+    if re.match(reg, typed_value):
+        try:
+            ipaddress.IPv4Network(ip_input_box.value)
+        except ValueError:
+            return False
+        else:
+            return True
 
-    if not ip_input_box.value:   # jesli nic nie jest wpisane ta czesc zatrzymuje sprawdzanie zeby nie marnowac zasobow
-        return
-    x = re.match(reg, ip_input_box.value)
-    if x == None:                # ukrycie przycisku przy wpisaniu błednego adresu
-        test_button.hide()
-    else:
-        test_button.show()               # ponowne pokazanie przycisku jesli adres ip zgadza sie z szablonem
-        ip_input_box.cancel(check_regex)
 # okienko pokazujące wykonane obliczenia
 def show_next_window():
     window = Window(app, title="Wynik")
@@ -22,6 +21,7 @@ def show_next_window():
     network_text = Text(window, network_net_text)
     network_broad_text = "Adres broadcast: " + str(network.broadcast_address)
     network_broad = Text(window, network_broad_text)
+    network_us = Text(window, text = "Użytkowe adresy znajdują sie między adresem ip a adresem broadcast")
     network_mask_text = "Maska sieci: " + str(network.netmask)
     network_mask = Text(window, network_mask_text)
     network_host_text = "Maksymalna liczba hostów: " + str(network.num_addresses)
@@ -33,15 +33,28 @@ def show_next_window():
     close = PushButton(window, text="Zamknij", command = close_window)
 
 
+def show_element(element):
+    element.show()
+
+
+def verify_proper_ip_address_and_mask_were_passed():
+    if is_proper_input(ip_input_box.value):
+        test_button.show()
+        error_text.hide()
+    else:
+        error_text.value = f'Błędnie wpisana sieć/maska - obliczenie nie możliwe'
+        error_text.show()
+        test_button.hide()
 
 # główna część aplikacji pozwalająca na wpisanie adresu ip
 app = App(title="Liczenie adresów IP")
 entry_text = Text(app, text="Podaj adres IP wraz z maską w formacie x.x.x.x/x")
 example_text = Text(app, text="Przykład: 192.161.1.0/25")
 ip_input_box = TextBox(app, width=20)
-test_button = PushButton(app, command=show_next_window)
-ip_input_box.repeat(1000, check_regex)
+ip_input_box.update_command(verify_proper_ip_address_and_mask_were_passed)
+test_button = PushButton(text='Oblicz', master=app, command=show_next_window)
+error_text = Text(app, text='Zacznij pisać...', color='red')
+test_button.hide()
 app.display()
-
 
 
